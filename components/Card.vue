ce13b0086e73a22b6700994b868708cf5e8f0eef
Jawad Ali <jawad.ali@repairdesk.co>
2024-08-07 17:45:07 +0500

    made basic card

---
+++ components/Card.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps({
+  cardData: {
+    type: Object,
+    default: null,
+  },
+})
+</script>
+
+<template>
+  <div
+    :style="`background-color: ${cardData.colors.colorBody}; color: ${cardData.colors.colorText}; position: absolute; top: ${cardData.position.y}px; left: ${cardData.position.x}px;`"
+    class="w-[400px] cursor-pointer rounded-md shadow"
+  >
+    <p>{{ cardData.body }}</p>
+  </div>
+</template>
+
+<style scoped>
+.shadow {
+  box-shadow:
+    0 1px 1px hsl(0deg 0% 0% / 0.075),
+    0 2px 2px hsl(0deg 0% 0% / 0.075),
+    0 4px 4px hsl(0deg 0% 0% / 0.075),
+    0 8px 8px hsl(0deg 0% 0% / 0.075),
+    0 16px 16px hsl(0deg 0% 0% / 0.075);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+const { data } = await useFetch('/api/fake-json')
 </script>
 
 <template>
   <div>
-    hi
+    <Card v-for="card in data" :key="card.id" :card-data="card" />
+    <!-- <pre>
+
+      {{ data }}
+    </pre> -->
   </div>
 </template>
