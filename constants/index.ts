1aa4ce10a7c56c4fc22f27cd74c874b7797aaad0
Jawad Ali <jawad.ali@repairdesk.co>
2024-08-07 08:13:58 +0500

    chore: init

---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const appName = 'Sticky Nuxt'
+export const appDescription = 'Sticky Nuxt (notes)'
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    hi
+  </div>
+</template>
