c3ceabbd967fc39c214a94649904806e1fc080e6
Jawad Ali <jawad.ali@repairdesk.co>
2025-05-14 07:35:05 +0500

    fix: ensure card key is a string in notes list

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@ async function updateNote(id: any, note: Note) {
   <div>
     <UButton icon="i-carbon:add-large" size="xl" color="primary" square class="absolute left-1 top-1" variant="link" @click="createNewNote" />
     <div class="overflow-auto p-40">
-      <Card v-for="card in notes" :key="card._id" :card-data="card" @delete="deleteNote(card._id)" @update="updateNote(card._id, $event)" />
+      <Card v-for="card in notes" :key="String(card._id)" :card-data="card" @delete="deleteNote(card._id)" @update="updateNote(card._id, $event)" />
     </div>
   </div>
 </template>
